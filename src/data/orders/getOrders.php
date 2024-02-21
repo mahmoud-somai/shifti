50fe702949e17cd9c111640734c2639562700157
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-02-21 16:46:58 +0100

    orders  commit

--- src/data/orders/getOrders.php
+++ src/data/orders/getOrders.php
@@ -36,7 +36,8 @@ function get_orders() {
         $orders_data[] = $order_data;
     }
 
-    // Encode orders data as JSON and output
+    // Encode orders data as JSON and output$
+    echo "hello world";
     echo json_encode($orders_data);
     echo '<script src="' . plugins_url('shifti-import/src/scripts/index.js') . '"></script>';
 }
