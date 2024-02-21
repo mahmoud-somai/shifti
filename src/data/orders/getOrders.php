de883a82e97a07e01142f356e968d8f947b3f161
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-02-21 17:50:39 +0100

    orders  commit

--- src/data/orders/getOrders.php
+++ src/data/orders/getOrders.php
@@ -6,43 +6,30 @@ function get_orders() {
 
     // SQL query to retrieve orders
     $args = array(
-        'limit' => 1, // -1 retrieves all orders
+        'limit' => -1, // -1 retrieves all orders
     );
 
     $orders_query = new WC_Order_Query($args);
     $orders = $orders_query->get_orders();
 
-   
-    echo $orders;
-   
-    echo sizeof($orders);
-    echo '<br />';
-    echo '<br />';
-    $orders_data =array();
+    echo "Number of Orders: " . sizeof($orders) . "<br>";
+    echo '<br>';
+    echo '<br>';
 
-    foreach ($orders as $result) {
-echo $result;
-        array_push($orders_data, $result);
+    $orders_data = array();
+
+    foreach ($orders as $order) {
+        $order_id = $order->get_id();
+        $orders_data[] = $order_id;
     }
-  
 
+    // Output order IDs
+    echo '<pre>';
+    print_r($orders_data);
+    echo '</pre>';
 }
 
-
-
+// Call the function to get orders
+get_orders();
 
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
