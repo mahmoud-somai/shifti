0715fc82b0d5e3a3dd97e0bba8db8ec5ae6ccdb8
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-02-21 18:03:31 +0100

    orders  commit

--- src/data/orders/getOrders.php
+++ src/data/orders/getOrders.php
@@ -18,13 +18,10 @@ function get_orders() {
 
     $orders_data = [];
 
-
     foreach ($orders as $order) {
         $order_id = $order->get_id();
-        $new_order_object['order_id'] = $order_id;
-        array_push($orders_data, $new_order_object);
-        echo $new_order_object;
-        
+ 
+        array_push($orders_data, $order_id);
     }
 
     // Output order IDs
