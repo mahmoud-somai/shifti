0147ed81e1c75d83fcbb7c8416c4997dd5d7dbeb
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-02-22 16:25:26 +0100

    orders  commit

--- src/data/orders/getOrders.php
+++ src/data/orders/getOrders.php
@@ -75,7 +75,7 @@ function get_orders() {
 
         $cart_hash = $order->get_cart_hash();
         $order_stock_reduced = $order->get_order_stock_reduced();
-    //    $download_permissions_granted = $order->get_download_permissions_granted();
+        $download_permissions_granted = $order->get_download_permissions_granted();
     //    $new_order_email_sent = $order->get_new_order_email_sent();
     //    $recorded_sales = $order->get_recorded_sales();
       //  $recorded_coupon_usage_counts = $order->get_recorded_coupon_usage_counts();
@@ -142,7 +142,7 @@ function get_orders() {
             'date_paid' => $date_paid,
             'cart_hash' => $cart_hash,
             'order_stock_reduced' => $order_stock_reduced,
-          // 'download_permissions_granted' => $download_permissions_granted,
+            'download_permissions_granted' => $download_permissions_granted,
          //   'new_order_email_sent' => $new_order_email_sent,
            // 'recorded_sales' => $recorded_sales,
            // 'recorded_coupon_usage_counts' => $recorded_coupon_usage_counts,
