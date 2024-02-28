0ad8a59e5d532382385eaf274dbfbe34d7e54969
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-02-28 10:56:39 +0100

    add fees  to orders  commit

--- src/data/orders/getOrders.php
+++ src/data/orders/getOrders.php
@@ -461,7 +461,7 @@ function get_orders() {
             $total           = $item->get_total();
             $tax_status      = $item->get_tax_status();
             echo "<br>";
-            echo "sku item line ===> ".$item['sku'];
+            echo "sku item line ===> ".$item->get_sku();
            
         
         }
