2997424662226437bb46abadd352e9159829ee58
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-02-29 16:23:46 +0100

    new commit

--- src/data/orders/getOrders.php
+++ src/data/orders/getOrders.php
@@ -97,6 +97,7 @@ function get_orders() {
             $tax_subtotal    = $item->get_subtotal_tax();
             $total           = $item->get_total();
             $tax_status      = $item->get_tax_status();
+            $total_tax       = $item->get_total_tax(); 
             $sku             = $item->get_product()->get_sku();
             $item_price      = $item->get_product()->get_price();
 
@@ -110,6 +111,7 @@ function get_orders() {
                 'subtotal_tax' => $tax_subtotal ,
                 'total' => $total ,
                 'tax status' => $tax_status ,
+                'total_tax' => $total_tax ,
                 'sku' => $sku, 
                 'price' => $item_price,
             );
