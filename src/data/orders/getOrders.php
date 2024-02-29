17ce144e15371495dec10ab431e5a8694b2491ec
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-02-29 11:27:16 +0100

    clean code of orders  commit

--- src/data/orders/getOrders.php
+++ src/data/orders/getOrders.php
@@ -76,7 +76,7 @@ function get_orders() {
         $shipping_phone = $order->get_shipping_phone() ?: null; // Assign null if empty
         
         $order_stock_reduced = $order->get_order_stock_reduced() ?: null;
-        $num_attributes = count(get_object_vars($order));
+
     
         $orders_data[] = array(
             'id' => $order_id,
@@ -129,7 +129,7 @@ function get_orders() {
             'order_stock_reduced' => $order_stock_reduced,
             'currency' => $order_currency,
             'order_number' => $order_number,
-            'num_attributes' => $num_attributes,
+          
         );
         
     }
@@ -137,6 +137,8 @@ function get_orders() {
     // Output orders data
     echo '<br> Orders Data: <br>';
     echo json_encode($orders_data);
+    echo '<br>';
+    echo sizeof($orders_data);
     echo '<br>';
 }
 
