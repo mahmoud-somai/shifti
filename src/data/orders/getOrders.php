507e911a53e7512c96f5a3ac18139136d6252464
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-02-29 12:21:54 +0100

    clean code of orders  commit

--- src/data/orders/getOrders.php
+++ src/data/orders/getOrders.php
@@ -12,12 +12,15 @@ function get_orders() {
     $orders_query = new WC_Order_Query($args);
     $orders = $orders_query->get_orders();
 
-
+$data=[];
     foreach ( $orders->get_items() as $item_id => $item ) {
-        echo $item;
+        echo json_encode($item) ;
         echo '<br>';
-
-}
+        array_push($data, $item);
+    }
+  
+echo json_encode($data);
+   
 }
 
 
