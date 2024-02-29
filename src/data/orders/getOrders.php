5c0c508da02d22852f79d509761a90c45acfb6bf
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-02-29 14:17:41 +0100

    new commit

--- src/data/orders/getOrders.php
+++ src/data/orders/getOrders.php
@@ -29,9 +29,9 @@ function get_orders() {
         $order_data['discount_tax'] = $order->get_discount_tax() ?? null;
         $order_data['shipping_total'] = $order->get_shipping_total() ?? null;
         $order_data['shipping_tax'] = $order->get_shipping_tax() ?? null;
-        // $order_data['cart_tax'] = $order->get_cart_tax() ?? null;
-        // $order_data['total'] = $order->get_total() ?? null;
-        // $order_data['total_tax'] = $order->get_total_tax() ?? null;
+        $order_data['cart_tax'] = $order->get_cart_tax() ?? null;
+        $order_data['total'] = $order->get_total() ?? null;
+        $order_data['total_tax'] = $order->get_total_tax() ?? null;
         // $order_data['customer_id'] = $order->get_customer_id() ?? null;
         // $order_data['order_key'] = $order->get_order_key() ?? null;
         
