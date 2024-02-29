4c04cf410215320ccaaf490c98727ca545c6fbac
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-02-29 12:26:07 +0100

    clean code of orders  commit

--- src/data/orders/getOrders.php
+++ src/data/orders/getOrders.php
@@ -6,20 +6,15 @@
 function get_orders() {
     global $wpdb;
     args = array(
-        'limit' => 1, // -1 retrieves all orders
+        'limit' => -1, // -1 retrieves all orders
     );
 
     $orders_query = new WC_Order_Query($args);
     $orders = $orders_query->get_orders();
 
 $data=[];
-    foreach ( $orders->get_items() as $item_id => $item ) {
-        echo json_encode($item) ;
-        echo '<br>';
-        array_push($data, $item);
-    }
   
-echo json_encode($data);
+echo json_encode($orders);
    
 }
 
