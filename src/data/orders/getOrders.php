37c93b85f8d05087979243f1a10ad24e6e0a8aa0
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-02-29 10:47:46 +0100

    clean code of orders  commit

--- src/data/orders/getOrders.php
+++ src/data/orders/getOrders.php
@@ -8,7 +8,7 @@ function get_orders() {
     // $args = array(
     //     'limit' => -1, 
     // );
-    $specific_order_ids = array(24); //50771
+    $specific_order_ids = array(25); //50771
 
 
     $args = array(
