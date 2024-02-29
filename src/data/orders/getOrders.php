2b18158f2ad07aa9ca35baa1247293dbe6089c05
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-02-29 15:06:27 +0100

    new commit

--- src/data/orders/getOrders.php
+++ src/data/orders/getOrders.php
@@ -32,11 +32,7 @@ function get_orders() {
         $order_data['total_tax'] = $order->get_total_tax() ?? null;
         $order_data['prices_include_tax'] = $order->get_prices_include_tax() ?? null;
 
-        try {
-            $order_data['customer_id'] = $order->get_customer_id();
-        } catch (Exception $e) {
-            $order_data['customer_id'] = null;
-        }
+ 
 
  
         // $order_data['customer_id'] = $order->get_customer_id() ?? null;
