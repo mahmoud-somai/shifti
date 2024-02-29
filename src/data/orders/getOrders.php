621871b4382fa6187b0516c3b4f407bd9f22975e
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-02-29 12:22:46 +0100

    clean code of orders  commit

--- src/data/orders/getOrders.php
+++ src/data/orders/getOrders.php
@@ -6,7 +6,7 @@
 function get_orders() {
     global $wpdb;
     args = array(
-        'limit' => -1, // -1 retrieves all orders
+        'limit' => 1, // -1 retrieves all orders
     );
 
     $orders_query = new WC_Order_Query($args);
