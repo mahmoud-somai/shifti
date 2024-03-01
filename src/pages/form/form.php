bf013f46af029e06f67fb909fcb9d4ddcd455c0e
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-03-01 16:18:26 +0100

    new commit

--- src/pages/form/form.php
+++ src/pages/form/form.php
@@ -14,7 +14,7 @@ function form_html(){
             echo '<label for="token">plugin token</label>';
             echo '<input type="text" name"plugin-token" id="token">';
         echo '</div>';
-        echo '<button class="button-sft" onclick="myFunction()">Import your shop!</button> ';
+        echo '<button class="button-sft" onclick="myFunction()">Import  shop!</button> ';
 
     echo '</div>';
     
