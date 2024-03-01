ceb2a2a2d4e4e6499e2e9c65881b567503bc65f6
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-03-01 12:21:23 +0100

    categories commit

--- src/data/categories/getCategories.php
+++ src/data/categories/getCategories.php
@@ -24,7 +24,9 @@ function get_ctg(){
 
    $category=[];
    foreach ($all_categories as $cat) {
-
+    echo "<br>";
+    echo json_encode($cat);
+    echo "<br>";
 
         $categories=[];
 
@@ -35,9 +37,6 @@ function get_ctg(){
         $categories['description']=$cat->description;
         $categories['display']=$cat->display;
         //$categories['image']=$cat->image_thumbnail;
-        // $cat_thumb_id = get_term_meta( $cat->term_id, 'thumbnail_id', true );
-        // $image = wp_get_attachment_url( $cat_thumb_id ); 
-        $category['image'] = $image;
         $categories['menu_order']=$cat->menu_order;
         $categories['count']=$cat->count;
         $category[]=$categories;  
@@ -47,9 +46,6 @@ function get_ctg(){
   echo "<br>";
   echo json_encode($category);
   echo "<br>";
-
-
-
 
 }
 ?>
