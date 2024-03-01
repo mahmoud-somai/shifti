2c21c46b3a7cdaef3af02f0131096f4ffc23d2a1
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-03-01 15:28:23 +0100

    categories commit

--- src/pages/home/home-page.php
+++ src/pages/home/home-page.php
@@ -17,9 +17,7 @@ function header_html(){
         echo '<h1 style="font-size: 24px; margin-top: 20px;">Welcome to Shifti WordPress Plugin</h1>';
         echo '<p style="font-size: 16px; margin-top: 10px;">Here you can import the data of your shop</p>';
         echo '<p style="font-size: 16px;">By entering your plugin shop secret key and pressing import, you can import your WooCommerce data to your shop!</p>';
-        echo '<form method="post" action="' . admin_url('admin-ajax.php') . '">';
-        echo '<input type="hidden" name="action" value="download_json">';
-        echo '<button type="submit">Download JSON</button>';
+        
         form_html();
     echo '</div>';
     
