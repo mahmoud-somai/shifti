8288f3d01c4b888d95ee6c35ddf409fcf6740408
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-03-06 16:26:52 +0100

    tax_rate_id  commit

--- src/data/taxes/getTaxes.php
+++ src/data/taxes/getTaxes.php
@@ -21,7 +21,7 @@ function get_txs(){
     if(!empty($tax_rates)){
         foreach ($tax_rates as $tax_rate) {
             echo "<br>";
-            echo json_encode($tax_rate->tax_rate_id);
+            echo json_encode($tax_rate->get_rate_id());
             echo "<br>";
             array_push($tab_rates,$tax_rate);
         }
