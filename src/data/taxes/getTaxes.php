f87ed69f050f991bb6d852da87a3ef636e5e67e1
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-03-07 10:20:05 +0100

    tax_rate_class th  commit

--- src/data/taxes/getTaxes.php
+++ src/data/taxes/getTaxes.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 function get_txs(){
 
     global $wpdb;
@@ -8,33 +10,57 @@ function get_txs(){
     $tab_tax=[];
     $tab_rates=[];
 
+
     if ( ! empty( $tax_classes ) ) {
-        // Check if there is at least a third tax class
-        if (count($tax_classes) > 2) {
-            // Get the third tax class
-            $third_tax_class = $tax_classes[2];
-            
-            // Get tax rates for the third tax class
-            $taxes = WC_Tax::get_rates_for_tax_class( $third_tax_class );
-            
-            // Store the tax class and rates
-            $tab_tax[] = $third_tax_class;
-            $tab_rates[] = $taxes;
-        } else {
-            // If there is no third tax class, return a message
-            echo "There is no third tax class.";
-            return;
+        foreach ( $tax_classes as $class ) {
+            $taxes = WC_Tax::get_rates_for_tax_class( $class );
+            array_push($tab_tax,$class);
+            array_push($tab_rates,$taxes);
         }
     }
-
-    // Output the tax class and rates
     echo "<h2>get_tax_classes:</h2>";
     echo json_encode($tab_tax);
     echo "<br>";
 
-    echo "<h2>get taxes new tests rates for third tax class:</h2>";
+    // if(!empty($tax_rates)){
+    //     foreach ($tax_rates as $tax_rate) {
+
+    //         array_push($tab_rates,$tax_rate);
+    //     }
+    // }
+
+    echo "<h2>get taxes new tests rates:</h2>";
     echo json_encode($tab_rates);
     echo "<br>";
 }
+
+// function get_txs(){
+
+//      global $wpdb;
+//      $tax_rates = WC_Tax::get_rates();
+//      echo "<h2>get_rates:</h2>";
+//      echo json_encode($tax_rates);
+//      echo "<br>";
+
+//      // Check if there are tax rates
+//      if (!empty($tax_rates)) {
+//          echo "<h2>All Tax Rates:</h2>";
+ 
+//          // Loop through each tax rate
+//          foreach ($tax_rates as $tax_rate) {
+//              echo "<br>";
+//              echo json_encode($tax_rate);
+//              echo "<br>";
+//              $rate_id=$tax_rate->get_tax_rate();
+//              echo "rate id ==> <br>";
+//              echo $rate_id;
+//              echo "<br>";
+
+//          }
+//      } else {
+//          echo "No tax rates found.";
+//      }
+
+// }
 
 ?>
