5dc236167f55b69cca4b6502a2bc018efa7d8885
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-03-07 12:26:12 +0100

    tax_rate_class th  commit

--- src/data/taxes/getTaxes.php
+++ src/data/taxes/getTaxes.php
@@ -24,7 +24,7 @@ function get_txs(){
             } else {
                 echo "No tax rates found for class: $class";
             }
-            $all_tax_rate_ids[$class] = $tax_rate_ids;
+            $all_tax_rate_ids[$"id"] = $tax_rate_ids;
         }
     } else {
         echo "No tax classes found.";
