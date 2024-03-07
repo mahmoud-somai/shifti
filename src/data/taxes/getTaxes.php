8f30e7fb8b8f38eca6f4e91833a447daa14b60f5
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-03-07 10:18:00 +0100

    tax_rate_class  commit

--- src/data/taxes/getTaxes.php
+++ src/data/taxes/getTaxes.php
@@ -1,66 +1,33 @@
 <?php
 
-
-
 function get_txs(){
 
     global $wpdb;
     $tax_classes           = WC_Tax::get_tax_classes();
-    $tax_rates              = WC_Tax::get_rates();
+    $tax_rates             = WC_Tax::get_rates();
     $tab_tax=[];
     $tab_rates=[];
 
-
     if ( ! empty( $tax_classes ) ) {
-        foreach ( $tax_classes as $class ) {
-            $taxes = WC_Tax::get_rates_for_tax_class( $class );
-            array_push($tab_tax,$class);
-            array_push($tab_rates,$taxes);
-        }
+        // Get the first tax class
+        $first_tax_class = reset($tax_classes);
+        
+        // Get tax rates for the first tax class
+        $taxes = WC_Tax::get_rates_for_tax_class( $first_tax_class );
+        
+        // Store the tax class and rates
+        $tab_tax[] = $first_tax_class;
+        $tab_rates[] = $taxes;
     }
+
+    // Output the tax class and rates
     echo "<h2>get_tax_classes:</h2>";
     echo json_encode($tab_tax);
     echo "<br>";
 
-    // if(!empty($tax_rates)){
-    //     foreach ($tax_rates as $tax_rate) {
-
-    //         array_push($tab_rates,$tax_rate);
-    //     }
-    // }
-
-    echo "<h2>get taxes new tests rates:</h2>";
+    echo "<h2>get taxes new tests rates for first tax class:</h2>";
     echo json_encode($tab_rates);
     echo "<br>";
 }
-
-// function get_txs(){
-
-//      global $wpdb;
-//      $tax_rates = WC_Tax::get_rates();
-//      echo "<h2>get_rates:</h2>";
-//      echo json_encode($tax_rates);
-//      echo "<br>";
-
-//      // Check if there are tax rates
-//      if (!empty($tax_rates)) {
-//          echo "<h2>All Tax Rates:</h2>";
- 
-//          // Loop through each tax rate
-//          foreach ($tax_rates as $tax_rate) {
-//              echo "<br>";
-//              echo json_encode($tax_rate);
-//              echo "<br>";
-//              $rate_id=$tax_rate->get_tax_rate();
-//              echo "rate id ==> <br>";
-//              echo $rate_id;
-//              echo "<br>";
-
-//          }
-//      } else {
-//          echo "No tax rates found.";
-//      }
-
-// }
 
 ?>
