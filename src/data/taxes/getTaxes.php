8b7065f48a157cc65c19531e4746f37f9db820e2
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-03-07 12:21:33 +0100

    tax_rate_class th  commit

--- src/data/taxes/getTaxes.php
+++ src/data/taxes/getTaxes.php
@@ -5,34 +5,42 @@ function get_txs(){
     global $wpdb;
     $tax_classes = WC_Tax::get_tax_classes();
     $tax_rates = WC_Tax::get_rates();
+    $all_tax_rate_ids = array(); // Array to store all tax rate IDs
 
     if (!empty($tax_classes)) {
         foreach ($tax_classes as $class) {
             $taxes = WC_Tax::get_rates_for_tax_class($class);
-            // Decode JSON string into an associative array
-            $tax_rate_ids = array();
+            $tax_rate_ids = array(); // Array to store tax rate IDs for current tax class
 
             echo "<h2>Tax Class: $class</h2>";
             echo json_encode($taxes);
             echo "<br>";
 
-            
+            if (!empty($taxes)) {
                 foreach ($taxes as $tax) {
                     // Extract tax rate ID
-                    array_push($tax_rate_ids, $tax->tax_rate_id);
+                    $tax_rate_ids[] = $tax->tax_rate_id;
                 }
-                // Output tax rate IDs for the current tax class
-                echo "<h3>Tax Rate IDs for $class</h3>";
-                echo json_encode($tax_rate_ids);
-                echo "<br>";
-     
+            } else {
+                echo "No tax rates found for class: $class";
+            }
+
+            // Output tax rate IDs for the current tax class
+            echo "<h3>Tax Rate IDs for $class</h3>";
+            echo json_encode($tax_rate_ids);
+            echo "<br>";
+
+            // Add tax rate IDs for the current tax class to the array of all tax rate IDs
+            $all_tax_rate_ids[$class] = $tax_rate_ids;
         }
     } else {
         echo "No tax classes found.";
     }
-                echo "<h3>Tax Rate IDs </h3>";
-                echo json_encode($tax_rate_ids);
-                echo "<br>";
+
+    // Output all tax rate IDs
+    echo "<h3>All Tax Rate IDs</h3>";
+    echo json_encode($all_tax_rate_ids);
+    echo "<br>";
 }
 
 ?>
