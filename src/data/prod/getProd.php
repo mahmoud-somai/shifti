7f95b0cd163196cf0d5727626d8f1590686a1beb
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-03-11 11:07:37 +0100

    prod  commit

--- src/data/prod/getProd.php
+++ src/data/prod/getProd.php
@@ -4,27 +4,14 @@ function get_prod(){
 
     global $wpdb;
 
-    $query = "
-      SELECT p.ID, p.post_type, p.post_title, p.post_content, p.post_excerpt, p.post_status
-      FROM wp_posts p
-      WHERE p.post_type = 'product'
-    ";
-    
-    $results = $wpdb->get_results($query);
-    
-    $params = array(
-        'posts_per_page' => 1,
-        'post_type' => 'product'
+    $args = array(
+        'limit' => -1,
+        'status'=> array( 'draft', 'pending', 'private', 'publish' ),
+
     );
 
-    $wc_query = new WP_Query($params); 
-    $products=$wc_query->get_posts();
-
-
+    $products = wc_get_products($args);
     
-    //$products = wc_get_products( array( 'status' => array( 'draft', 'pending', 'private', 'publish' ), 'limit' => -1 ) );
-    
-
     echo "<h2>Products</h2>";
     echo json_encode($products);
     echo "<br>";
