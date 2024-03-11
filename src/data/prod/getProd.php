7386d767ce56828146e7338546cf22109fc2a799
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-03-11 10:27:03 +0100

    prod  commit

--- src/data/prod/getProd.php
+++ src/data/prod/getProd.php
@@ -18,6 +18,16 @@ function get_prod(){
         $temp_prod['name']=$product->get_name();
         $temp_prod['slug']=$product->get_slug();
         $temp_prod['permalink']=$product->get_permalink();
+        $temp_prod['date_created']=$product->get_date_created()->date('Y-m-d H:i:s');
+        $temp_prod['date_modified']=$product->get_date_modified()->date('Y-m-d H:i:s');
+        $temp_prod['type']=$product->get_type(); 
+        $temp_prod['status']=$product->get_status();
+        $temp_prod['catalog visibility']=$product->get_catalog_visibility();
+        $temp_prod['description']=$product->get_description();
+
+
+
+
 
         $tab_prod[] = $temp_prod;
 
