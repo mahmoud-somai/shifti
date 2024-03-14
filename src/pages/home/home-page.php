5fcfeb05ad3852be260d26bf073dafe6ada4501e
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-03-14 10:00:30 +0100

    connect with golang  commit

--- src/pages/home/home-page.php
+++ src/pages/home/home-page.php
@@ -52,30 +52,29 @@ function header_html(){
 
 
 
-        echo '<script type="text/javascript">';
-        echo 'function fetchOrdersNotesFromAPI() {';
-        echo '    jQuery.ajax({';
-        echo '        url: "' . admin_url('admin-ajax.php') . '",'; // URL of your WordPress AJAX endpoint
-        echo '        method: "POST",'; // Make a POST request
-        echo '        data: {'; // Data to send with the request
-        echo '            action: "send_orders_notes_to_api"'; // Specify the action for your AJAX handler
-        echo '        },';
-        echo '        success: function(response) {';
-        echo '            console.log("Data fetched successfully:", response);'; // Log success message
-        echo '            console.log("Data fetched successfully:", response.data[0]);'; 
-        echo '        },';
-        echo '        error: function(xhr, status, error) {';
-        echo '            console.error("Error fetching data:", error);'; // Log error message
-        echo '        }';
-        echo '    });';
-        echo '}';
-        echo '</script>';
+        // echo '<script type="text/javascript">';
+        // echo 'function fetchOrdersNotesFromAPI() {';
+        // echo '    jQuery.ajax({';
+        // echo '        url: "' . admin_url('admin-ajax.php') . '",'; // URL of your WordPress AJAX endpoint
+        // echo '        method: "POST",'; // Make a POST request
+        // echo '        data: {'; // Data to send with the request
+        // echo '            action: "send_orders_notes_to_api"'; // Specify the action for your AJAX handler
+        // echo '        },';
+        // echo '        success: function(response) {';
+        // echo '            console.log("Data fetched successfully:", response);'; // Log success message
+        // echo '        },';
+        // echo '        error: function(xhr, status, error) {';
+        // echo '            console.error("Error fetching data:", error);'; // Log error message
+        // echo '        }';
+        // echo '    });';
+        // echo '}';
+        // echo '</script>';
         
 
-        // Form to trigger sending orders notes to API
-        echo '<form>';
-        echo '<button type="button" onclick="fetchOrdersNotesFromAPI()">Send Orders Notes to API </button>';
-        echo '</form>';
+        // // Form to trigger sending orders notes to API
+        // echo '<form>';
+        // echo '<button type="button" onclick="fetchOrdersNotesFromAPI()">Send Orders Notes to API </button>';
+        // echo '</form>';
 
 
    
