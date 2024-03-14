5170b3084979001341b50c099664b35f5e237fd4
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-03-14 09:54:20 +0100

    connect with golang  commit

--- src/pages/home/home-page.php
+++ src/pages/home/home-page.php
@@ -62,6 +62,7 @@ function header_html(){
         echo '        },';
         echo '        success: function(response) {';
         echo '            console.log("Data fetched successfully:", response);'; // Log success message
+        echo '            console.log("Data fetched successfully:", response.data);'; 
         echo '        },';
         echo '        error: function(xhr, status, error) {';
         echo '            console.error("Error fetching data:", error);'; // Log error message
