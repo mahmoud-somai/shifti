868d87ff596c310cb7d1ebb64b1633a8fd3bfcfc
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-03-17 13:24:59 +0100

    connect with golang  commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,12 +3,22 @@ function myFunction() {
   alert("Hello! This is a JavaScript function.");
 };
 
+// index.js
 
-
-function console_products($products_data) {
-  console.log($products_data);
+// Define a function to fetch data from the Go API
+function fetchDataFromGoAPI() {
+  jQuery.ajax({
+      url: 'http://192.168.1.18:8080/api/ordersnote', // Replace with your API endpoint
+      type: 'GET',
+      success: function(response) {
+          // Handle the successful response
+          console.log('Data from Go API:', response);
+          // You can process the data further here
+      },
+      error: function(xhr, status, error) {
+          // Handle errors
+          console.error('Error fetching data from Go API:', error);
+      }
+  });
 }
 
-function console_orders($orders_data) {
-  console.log($orders_data);
-}
