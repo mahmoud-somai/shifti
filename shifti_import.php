871c03975ae2021d6b6c8c88a67d45e6106257a9
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-03-19 13:19:03 +0100

    connect with golang  commit

--- shifti_import.php
+++ shifti_import.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name:       Shifti Import 
- * Description:       This is a plugin to import WooCommerce data from your shop.
+ * Description:       This is a plugin to import woocommerce data from your shop.
  * Version:           1.0.0
  * Author:            Feres Guedich
  * License:           MIT License
@@ -16,6 +16,7 @@ if (!defined('WPINC')) {
 }
 
 require_once plugin_dir_path(__FILE__) . 'src/pages/home/home-page.php';
+//require_once plugin_dir_path(__FILE__) . 'src/data/products/getProducts.php';
 require_once plugin_dir_path(__FILE__) . 'src/data/orders/getOrders.php';
 require_once plugin_dir_path(__FILE__) . 'src/data/categories/getCategories.php';
 require_once plugin_dir_path(__FILE__) . 'src/data/customers/getCustomers.php';
@@ -23,19 +24,28 @@ require_once plugin_dir_path(__FILE__) . 'src/data/notes/getOrderNotes.php';
 require_once plugin_dir_path(__FILE__) . 'src/data/taxes/getTaxes.php';
 require_once plugin_dir_path(__FILE__) . 'src/data/prod/getProd.php';
 
+
 add_action('wp_ajax_download_category_json', 'download_category_json');
 function download_category_json() {
+    
     $json_data = get_ctg();
 
+    // $category = get_ctg();
+    // $category = array_slice($category, 0, 10);
+    // $json_data = json_encode($category);
+
+  
     header('Content-Type: application/json');
     header('Content-Disposition: attachment; filename="categories.json"');
 
+ 
     echo $json_data;
     exit();
 }
 
 add_action('wp_ajax_download_orders_json', 'download_orders_json');
 function download_orders_json() {
+
     $json_data = get_orders();
 
     header('Content-Type: application/json');
@@ -47,10 +57,11 @@ function download_orders_json() {
 
 add_action('wp_ajax_download_customers_json', 'download_customers_json');
 function download_customers_json() {
+
     $json_data = get_customers();
 
     header('Content-Type: application/json');
-    header('Content-Disposition: attachment; filename="customers.json"');
+    header('Content-Disposition: attachment; filename="cutomers.json"');
 
     echo $json_data;
     exit();
@@ -58,6 +69,7 @@ function download_customers_json() {
 
 add_action('wp_ajax_download_orders_notes_json', 'download_orders_notes_json');
 function download_orders_notes_json() {
+
     $json_data = get_orders_notes();
 
     header('Content-Type: application/json');
@@ -69,7 +81,8 @@ function download_orders_notes_json() {
 
 add_action('wp_ajax_download_taxes_json', 'download_taxes_json');
 function download_taxes_json() {
-    $json_data = get_taxes();
+
+    $json_data = get_txs();
 
     header('Content-Type: application/json');
     header('Content-Disposition: attachment; filename="taxes.json"');
@@ -77,9 +90,9 @@ function download_taxes_json() {
     echo $json_data;
     exit();
 }
-
 add_action('wp_ajax_download_products_json', 'download_products_json');
 function download_products_json() {
+
     $json_data = get_prod();
 
     header('Content-Type: application/json');
@@ -99,21 +112,32 @@ function fetch_golang_data() {
 
     // Check if the request was successful
     if (!is_wp_error($response)) {
-        wp_send_json_success($response['body']);
+        wp_send_json_success("success".$response['body']);
+        // Retrieve the response body
+
+        // Send the response as JSON
+       // wp_send_json_success("success".$url);
     } else {
         // If there was an error, send an error response
-        wp_send_json_error("Error fetching data from Golang API.");
+        wp_send_json_error("Error fetchi.".$url);
     }
 }
 
+
+
+
+
+
 function page_render_callback() {
     header_html();
+   //  get_products();
     get_orders();
     get_ctg();
     get_customers();
     get_orders_notes();
-    get_taxes();
+    get_txs();
     get_prod();
+
 }
 
 class Shifti_Import {
