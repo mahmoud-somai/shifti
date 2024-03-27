4a81dda9dba88e7f7af92a2c6f00095ea9e2c67e
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-03-27 10:31:21 +0100

    desc commit

--- src/pages/home/home-page.php
+++ src/pages/home/home-page.php
@@ -6,7 +6,7 @@ function header_html(){
     echo '<link rel="stylesheet" href="' . plugins_url( 'shifti-import/src/styles/main.css') . '">';
     echo '<div style="text-align: center; padding: 20px; background-color: #f0f0f0;">';
     echo '<img src="' . plugins_url( 'shifti-import/src/img/logo.png') . '" alt="Logo" style="width: 150px; height: 100px;">';
-    echo '<h1 style="font-size: 24px; margin-top: 20px;">Welcome to Shifti WordPress Plugin</h1>';
+    echo '<h1 style="font-size: 24px; margin-top: 20px;">Shifti Data Connector</h1>';
     echo '<p style="font-size: 16px; margin-top: 10px;">Here you can import the data of your shop</p>';
     echo '<p style="font-size: 16px;">By entering your plugin shop secret key and pressing import, you can import your WooCommerce data to your shop!</p>';
    
