552075a4e1df6894d94337dbea91908c7439e138
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-03-27 11:51:07 +0100

    desc commit

--- src/pages/form/form.php
+++ src/pages/form/form.php
@@ -5,14 +5,15 @@
 function form_html(){
     echo '<link rel="stylesheet" href="' . plugins_url( 'shifti-import/src/styles/main.css') . '">';
 
-    echo '<div class="stf-form" style="box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); padding: 20px; margin: 20px; background-color: white;">';
-    echo '<div class="form-group">';
-    echo '<label for="token" style="font-size: 16px; color: #333; padding-bottom: 5px;">Plugin Token:</label>';
-    echo '<input type="text" name"plugin-token" id="token" style="padding: 8px; margin-top: 5px; border: 1px solid #ccc; border-radius: 4px; width: 100%;">';
+    echo '<div class="stf-form" style="box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); padding: 20px; margin: 20px auto; background-color: white; width: 50%; text-align: center;">';
+    echo '<div class="form-group" style="display: inline-block; margin-bottom: 15px;">';
+    echo '<label for="token" style="font-size: 16px; color: #333; display: inline-block; width: 30%;">Plugin Token:</label>';
+    echo '<input type="text" name"plugin-token" id="token" style="padding: 8px; margin-left: 10px; border: 1px solid #ccc; border-radius: 4px; width: 60%; display: inline-block;">';
     echo '</div>';
-    echo '<button class="button-sft" style="background-color: #4CAF50; color: white; padding: 10px 20px; border: none; border-radius: 4px; cursor: pointer; margin-top: 10px;" onclick="myFunction()">Import Your Shop!</button>';
+    echo '<button class="button-sft" style="background-color: #4CAF50; color: white; padding: 10px 20px; border: none; border-radius: 4px; cursor: pointer;">Import Your Shop!</button>';
     echo '</div>';
 
     echo '<script src="' . plugins_url( 'shifti-import/src/scripts/index.js') . '"></script>';
 }
+
 
