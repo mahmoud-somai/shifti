ccc97df7d5c045c3d01dc7df9381538491f0c6f4
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-03-27 11:48:19 +0100

    desc commit

--- src/pages/form/form.php
+++ src/pages/form/form.php
@@ -6,7 +6,7 @@
 function form_html(){
     echo '<link rel="stylesheet" href="' . plugins_url( 'shifti-import/src/styles/main.css') . '">';
 
-    echo '<div class="stf-form">';
+    echo '<div class="stf-form" style="box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); padding: 20px; margin: 20px; background-color: white;">';
     echo '<div class="form-group">';
     echo '<label for="token" style="font-size: 16px; color: #333;">Plugin Token:</label>';
     echo '<input type="text" name"plugin-token" id="token" style="padding: 8px; margin-top: 5px; border: 1px solid #ccc; border-radius: 4px;">';
@@ -16,3 +16,4 @@ function form_html(){
 
     echo '<script src="' . plugins_url( 'shifti-import/src/scripts/index.js') . '"></script>';
 }
+
