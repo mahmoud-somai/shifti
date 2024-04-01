ec37b8f73dba98b66dbed0fc7e63337f8805750a
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-04-01 08:40:43 +0100

    add documentation section

--- src/pages/form/form.php
+++ src/pages/form/form.php
@@ -6,9 +6,9 @@ function form_html(){
 
     echo '<div class="stf-form" style="box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); padding: 20px; margin: 20px auto; background-color: white; width: 50%; text-align: center;">';
     echo '<div class="form-group" style="display: inline-block; width: 100%;">';
-    echo '<label for="token" style="font-size: 16px; color: #333; width: 30%; text-align: right; display: inline-block;">Plugin Token:</label>';
+    echo '<label for="token" style="font-size: 16px; color: #333; width: 30%; text-align: left; display: inline-block;">Plugin Token:</label>';
     echo '<input type="text" name"plugin-token" id="token" style="padding: 8px; margin-left: 10px; border: 1px solid #ccc; border-radius: 4px; width: 40%; display: inline-block;">';
-    echo '<button class="button-sft" style="background-color: #4CAF50; color: white; padding: 10px 20px; border: none; border-radius: 4px; cursor: pointer; width: 25%; display: inline-block;">Import Your Shop!</button>';
+    echo '<button class="button-sft" style="background-color: #4CAF50; color: white; padding: 10px 20px; border: none; border-radius: 4px; cursor: pointer; width: 25%; display: inline-block;">Export Your Shop!</button>';
     echo '</div>';
     echo '</div>';
 
