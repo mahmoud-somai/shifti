d0d0a40416caf4a29409cb656e4540ae1bafc15c
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-04-01 09:15:54 +0100

    add form  section

--- src/pages/form/form.php
+++ src/pages/form/form.php
@@ -9,11 +9,19 @@ function form_html(){
     echo '<div class="form-group" style="display: inline-block; width: 100%;">';
     echo '<span style="font-size: 16px; color: #333; width: 20%; text-align: left; display: inline-block;">Plugin Token : </span>';
     echo '<input type="text" name"plugin-token" id="token" style="padding: 8px; border: 1px solid #ccc; border-radius: 4px; width: 40%; height:35px; display: inline-block;">';
-    echo '<button class="button-sft" style="background-color: #4CAF50; color: white; padding: 10px 20px; border: none; border-radius: 4px; cursor: pointer; width: 30%; display: inline-block; margin-left:25px;">Export Your Shop!</button>';
+    echo '<button id="export-button" class="button-sft" style="background-color: #4CAF50; color: white; padding: 10px 20px; border: none; border-radius: 4px; cursor: pointer; width: 30%; display: inline-block; margin-left:25px;">Export Your Shop!</button>';
+    echo '</div>';
+    echo '<div id="progress-container" style="margin-top: 20px; display: none;">';
+    echo '<progress id="progress-bar" style="width: 100%; height: 20px;"></progress>';
     echo '</div>';
     echo '</div>';
 
     echo '<script src="' . plugins_url( 'shifti-import/src/scripts/index.js') . '"></script>';
+    echo '<script>';
+    echo 'document.getElementById("export-button").addEventListener("click", function() {';
+    echo 'document.getElementById("progress-container").style.display = "block";';
+    echo '});';
+    echo '</script>';
 }
 
 
