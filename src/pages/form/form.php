0c11f8352c13c6955e34261fd0592b172f0e5a8e
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-04-01 11:50:37 +0100

    add form  section

--- src/pages/form/form.php
+++ src/pages/form/form.php
@@ -22,14 +22,15 @@
 function form_html(){
     echo '<link rel="stylesheet" href="' . plugins_url( 'shifti-import/src/styles/main.css') . '">';
 
-    echo '<form>'; // Start the form tag
+    echo '<form method="post" action="' . admin_url('admin-ajax.php') . '">'; // Start the form tag
 
     echo '<div class="stf-form" style="box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); padding: 20px; margin: 20px auto; background-color: white; width: 50%; text-align: center;">';
     echo '<h2 style="width: 100%; margin-bottom: 10px;">Link Shop</h2>'; 
     echo '<div class="form-group" style="display: inline-block; width: 100%;">';
     echo '<span style="font-size: 16px; color: #333; width: 20%; text-align: left; display: inline-block;">Plugin Token : </span>';
     echo '<input type="text" name"plugin-token" id="token" style="padding: 8px; border: 1px solid #ccc; border-radius: 4px; width: 40%; height:35px; display: inline-block;">';
-    echo '<button id="export-button" class="button-sft" style="background-color: #4CAF50; color: white; padding: 10px 20px; border: none; border-radius: 4px; cursor: pointer; width: 30%; display: inline-block; margin-left:25px;">Export Your Shop!</button>';
+    echo '<input type="hidden" name="action" value="download_category_json">';
+    echo '<button type="submit" id="export-button" class="button-sft" style="background-color: #4CAF50; color: white; padding: 10px 20px; border: none; border-radius: 4px; cursor: pointer; width: 30%; display: inline-block; margin-left:25px;">Export Your Shop!</button>';
     echo '</div>';
     echo '<div id="progress-container" style="margin-top: 20px; display: none;">';
     echo '<progress id="progress-bar" style="width: 100%; height: 20px;"></progress>';
