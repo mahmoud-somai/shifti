3ca0528fe6d99a288892af9ba04d1b3eaca4cbfb
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-04-01 09:09:02 +0100

    add documentation section

--- src/pages/form/form.php
+++ src/pages/form/form.php
@@ -5,7 +5,7 @@ function form_html(){
     echo '<link rel="stylesheet" href="' . plugins_url( 'shifti-import/src/styles/main.css') . '">';
 
     echo '<div class="stf-form" style="box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); padding: 20px; margin: 20px auto; background-color: white; width: 50%; text-align: center;">';
-    echo '<h2> Link Your Shop </h2>';
+    echo '<h2 style="text-align: center;">Link Shop</h2>';
     echo '<div class="form-group" style="display: inline-block; width: 100%;">';
     echo '<span style="font-size: 16px; color: #333; width: 20%; text-align: left; display: inline-block;">Plugin Token : </span>';
     echo '<input type="text" name"plugin-token" id="token" style="padding: 8px; border: 1px solid #ccc; border-radius: 4px; width: 40%; height:35px; display: inline-block;">';
