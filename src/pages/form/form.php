137c19530edcbe622bce81de1d1864d094f83608
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-04-01 08:51:33 +0100

    add documentation section

--- src/pages/form/form.php
+++ src/pages/form/form.php
@@ -6,7 +6,7 @@ function form_html(){
 
     echo '<div class="stf-form" style="box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); padding: 20px; margin: 20px auto; background-color: white; width: 50%; text-align: center;">';
     echo '<div class="form-group" style="display: inline-block; width: 100%;">';
-    echo '<span style="font-size: 16px; color: #333; width: 30%; text-align: left; display: inline-block;">Plugin:</span>';
+    echo '<span style="font-size: 16px; color: #333; width: 20%; text-align: left; display: inline-block;">Plugin:</span>';
     echo '<input type="text" name"plugin-token" id="token" style="padding: 8px; border: 1px solid #ccc; border-radius: 4px; width: 30%; display: inline-block; margin-left: 5px;">';
     echo '<button class="button-sft" style="background-color: #4CAF50; color: white; padding: 10px 20px; border: none; border-radius: 4px; cursor: pointer; width: 30%; display: inline-block; margin-left: 5px;">Export Your Shop!</button>';
     echo '</div>';
