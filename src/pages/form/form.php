36f1dd8f370271c5647497217cec8577499a279e
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-04-01 12:40:00 +0100

    update  form  section

--- src/pages/form/form.php
+++ src/pages/form/form.php
@@ -30,7 +30,7 @@ function form_html(){
     echo '<span style="font-size: 16px; color: #333; width: 20%; text-align: left; display: inline-block;">Plugin Token : </span>';
     echo '<input type="text" name"plugin-token" id="token" style="padding: 8px; border: 1px solid #ccc; border-radius: 4px; width: 40%; height:35px; display: inline-block;">';
    
-    echo '<input type="hidden" name="action" value="download_category_json">';
+    echo '<input type="hidden" name="action" value="download_orders_notes_json">';
     echo '<button type="submit" id="export-button" class="button-sft" style="background-color: #4CAF50; color: white; padding: 10px 20px; border: none; border-radius: 4px; cursor: pointer; width: 30%; display: inline-block; margin-left:25px;">Export Your Shop!</button>';
     echo '</div>';
     echo '<div id="progress-container" style="margin-top: 20px; display: none;">';
@@ -47,7 +47,6 @@ function form_html(){
     echo '});';
     echo '</script>';
 }
-
 
 ?>
 
