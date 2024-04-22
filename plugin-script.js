9b1e67d64175c3366af22fe11c41b56fa57a4d78
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-04-22 12:49:22 +0100

    try connection

--- plugin-script.js
+++ plugin-script.js
@@ -1,18 +1,17 @@
 jQuery(document).ready(function($) {
     $("#fetch-golang-data-form").submit(function(event) {
         event.preventDefault(); // Prevent the default form submission
+        
         $.ajax({
-            url: "http://192.168.1.27:8080/api/ordersnote", // Specify HTTP explicitly
-            method: "GET", 
+            url: "http://192.168.1.27:8080/api/ordersnote",
+            method: "GET",
             success: function(response) {
-                // Log the endpoint called and response received
-                $("#endpoint-called").text("Endpoint called: ", this.url);
+                // Display the response received on the page
                 $("#response-received").text("Response received: " + response);
             },
             error: function(xhr, status, error) {
                 // Log an error message to the console
-                console.log("Error fetching: " + this.url);
-               
+                console.error("Error fetching: " + this.url);
             }
         });
     });
