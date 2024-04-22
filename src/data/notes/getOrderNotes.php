4f7ba498f101c8e3c6f34507ec35d1983a1deb42
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-04-22 23:44:14 +0100

    try connection

--- src/data/notes/getOrderNotes.php
+++ src/data/notes/getOrderNotes.php
@@ -8,7 +8,7 @@ function get_orders_notes(){
         SELECT *
         FROM $table_perfixed
         WHERE  `comment_type` LIKE  'order_note'
-        and 'comment_ID' = 28
+        and 'comment_ID' > 31
 
     ");
 
