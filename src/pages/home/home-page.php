8e002e6ba9120d4c63afd57c32f837a9ca7c8ed4
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-04-22 09:58:17 +0100

    try connection

--- src/pages/home/home-page.php
+++ src/pages/home/home-page.php
@@ -57,8 +57,9 @@ function header_html(){
    
     echo '<form id="fetch-golang-data-form">';
     echo '<input type="hidden" name="action" value="fetch_golang_data">'; // Set the action to call fetch_golang_data
-    echo '<button type="submit" id="fetch-golang-data-button">Fetch Data from  DB</button>';
+    echo '<button type="button" id="fetch-golang-data-button">Fetch Data from DB</button>'; // Change type to button
     echo '</form>';
+    
     
 
     echo '</div>';
