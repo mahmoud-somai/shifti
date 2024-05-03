d72a04f0748f9fed43f19e9540486d15b557f167
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-05-03 09:33:53 +0100

    progress Bar

--- src/pages/home/home-page.php
+++ src/pages/home/home-page.php
@@ -19,12 +19,12 @@ function header_html(){
 
     // Add French documentation download link
     echo '<li style="margin-bottom: 20px; padding-left: 10px;">';
-    echo '<a href="' . plugins_url( 'shifti-import/sample.pdf') . '" download style="font-size: 15px; padding: 15px 10px; text-decoration: underline;">French Documentation</a>';
+    echo '<a href="' . plugins_url( 'shifti-import/sample.pdf') . '" download style="font-size: 15px; padding: 15px 10px; text-decoration: underline;">French</a>';
     echo '</li>';
 
     // Add English documentation download link
     echo '<li style="margin-bottom: 20px; padding-left: 10px;">';
-    echo '<a href="' . plugins_url( 'shifti-import/sample.pdf') . '" download style="font-size: 15px; padding: 15px 10px; text-decoration: underline;">English Documentation</a>';
+    echo '<a href="' . plugins_url( 'shifti-import/sample.pdf') . '" download style="font-size: 15px; padding: 15px 10px; text-decoration: underline;">English</a>';
     echo '</li>';
 
     echo '</ul>';
