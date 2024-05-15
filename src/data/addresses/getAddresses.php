efe99200129e36ba6e2faf16b5ea56bc5d3d5a32
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-05-15 10:57:34 +0100

    new commit 2

--- src/data/addresses/getAddresses.php
+++ src/data/addresses/getAddresses.php
@@ -13,17 +13,16 @@ function get_addresses() {
 
 
         $billing = [];
-
+        $billing['foreign_id'] = method_exists($order, 'get_customer_id') ? $order->get_customer_id() : null;
         $billing['first_name'] = method_exists($order, 'get_billing_first_name') ? $order->get_billing_first_name() : null;
         $billing['last_name'] = method_exists($order, 'get_billing_last_name') ? $order->get_billing_last_name() : null;
         $billing['company'] = method_exists($order, 'get_billing_company') ? $order->get_billing_company() : null;
         $billing['address_1'] = method_exists($order, 'get_billing_address_1') ? $order->get_billing_address_1() : null;
         $billing['address_2'] = method_exists($order, 'get_billing_address_2') ? $order->get_billing_address_2() : null;
         $billing['city'] = method_exists($order, 'get_billing_city') ? $order->get_billing_city() : null;
-        $billing['state'] = method_exists($order, 'get_billing_state') ? $order->get_billing_state() : null;
+        $billing['woo_state'] = method_exists($order, 'get_billing_state') ? $order->get_billing_state() : null;
         $billing['postcode'] = method_exists($order, 'get_billing_postcode') ? $order->get_billing_postcode() : null;
-        $billing['country'] = method_exists($order, 'get_billing_country') ? $order->get_billing_country() : null;
-        $billing['email'] = method_exists($order, 'get_billing_email') ? $order->get_billing_email() : null;
+        $billing['woo_country'] = method_exists($order, 'get_billing_country') ? $order->get_billing_country() : null;
         $billing['phone'] = method_exists($order, 'get_billing_phone') ? $order->get_billing_phone() : null;
 
 
