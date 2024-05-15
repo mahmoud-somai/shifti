ccf0b3aafd4081595b3708b85dfd074fb0fadbc9
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-05-15 12:18:02 +0100

    new commit 2

--- src/data/addresses/getAddresses.php
+++ src/data/addresses/getAddresses.php
@@ -14,8 +14,8 @@ function get_customers_with_billing() {
         $customer = [];
         $customer['foreign_id'] = $user->ID;
         $customer['email'] = $user->user_email;
-        $customer['first_name'] = $user->first_name;
-        $customer['last_name'] = $user->last_name;
+        $customer['first_name'] = $user->first_name ?: null;
+        $customer['last_name'] = $user->last_name ?: null;
 
         // Get billing information for this customer
         $billing_info = get_billing_info($user->ID);
@@ -28,6 +28,7 @@ function get_customers_with_billing() {
     return json_encode($customers);
 }
 
+
 function get_billing_info($customer_id) {
     $args = array(
         'customer_id' => $customer_id,
@@ -38,16 +39,17 @@ function get_billing_info($customer_id) {
 
     foreach ($orders as $order) {
         $billing = [];
-        $billing['first_name'] = $order->get_billing_first_name();
-        $billing['last_name'] = $order->get_billing_last_name();
-        $billing['company'] = $order->get_billing_company();
-        $billing['address_1'] = $order->get_billing_address_1();
-        $billing['address_2'] = $order->get_billing_address_2();
-        $billing['city'] = $order->get_billing_city();
-        $billing['woo_state'] = $order->get_billing_state();
-        $billing['postcode'] = $order->get_billing_postcode();
-        $billing['woo_country'] = $order->get_billing_country();
-        $billing['phone'] = $order->get_billing_phone();
+        // Get billing information attributes
+        $billing['first_name'] = $order->get_billing_first_name() ?: null;
+        $billing['last_name'] = $order->get_billing_last_name() ?: null;
+        $billing['company'] = $order->get_billing_company() ?: null;
+        $billing['address_1'] = $order->get_billing_address_1() ?: null;
+        $billing['address_2'] = $order->get_billing_address_2() ?: null;
+        $billing['city'] = $order->get_billing_city() ?: null;
+        $billing['woo_state'] = $order->get_billing_state() ?: null;
+        $billing['postcode'] = $order->get_billing_postcode() ?: null;
+        $billing['woo_country'] = $order->get_billing_country() ?: null;
+        $billing['phone'] = $order->get_billing_phone() ?: null;
 
         // Add billing info to the array
         $billing_info[] = $billing;
