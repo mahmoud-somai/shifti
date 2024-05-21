1be2c50227d23e3ae4e154d3c332a5c73da3b72a
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-05-21 10:39:41 +0100

    get taxes

--- src/data/taxes/getTaxes.php
+++ src/data/taxes/getTaxes.php
@@ -4,15 +4,7 @@ function get_txs(){
 
     global $wpdb;
     $tax_classes = WC_Tax::get_tax_classes();
-    $tax_st = WC_Tax::get_rates( 'service taxe' );
-
-    // Check if the tax rates array is empty
-    if ( ! empty( $tax_st ) ) {
-        echo 'true';
-    } else {
-        echo 'false';
-    }
-
+    <?php print_r($woocommerce->get('taxes')); ?>
     $tax_rates = WC_Tax::get_rates();
     $all_tax_rate_ids = array(); // Array to store all tax rate IDs with country
 
