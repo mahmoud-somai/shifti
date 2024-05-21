4ce834c8c49e3c62b1fb9174919bbbdff24118c2
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-05-21 11:17:41 +0100

    get standard class

--- src/data/taxes/getTaxes.php
+++ src/data/taxes/getTaxes.php
@@ -1,92 +1,42 @@
 <?php
 
-function get_txs(){
+function get_txs() {
+    // Ensure WooCommerce functions are loaded
+    if ( ! class_exists( 'WC_Tax' ) ) {
+        return json_encode([]);
+    }
 
-    global $wpdb;
+    // Get all tax classes
     $tax_classes = WC_Tax::get_tax_classes();
-    $tax_classes[] = '';
+    $tax_classes[] = ''; // Add the standard tax class
 
-  
-    $tax_rates = WC_Tax::get_rates();
- 
+    $all_tax_rate_info = array(); // Array to store all tax rate info
 
-    $all_tax_rate_ids = []; // Array to store all tax rate IDs with country
+    // Loop through each tax class
+    foreach ($tax_classes as $class) {
+        // Get tax rates for the current tax class
+        $taxes = WC_Tax::get_rates_for_tax_class($class);
 
-    if (!empty($tax_classes)) {
-        foreach ($tax_classes as $class) {
-            $taxes = WC_Tax::get_rates_for_tax_class($class);
-            // $tax_rate_info = array(); // Array to store tax rate info for current tax class
-        
-                foreach ($taxes as $tax) {
-                    $all_tax_rate_ids['foreign_id']=$tax->tax_rate_id,
-                    $all_tax_rate_ids['rate']=$tax->tax_rate,
-                    $all_tax_rate_ids['name']=$tax->tax_rate_name,
-                    $all_tax_rate_ids['class']=$tax->tax_rate_class,
-                }
-            
+        // Loop through each tax rate in the current tax class
+        foreach ($taxes as $tax) {
+            // Store only the required tax rate information
+            $tax_rate_info = array(
+                "id" => $tax->tax_rate_id,
+                "name" => $tax->tax_rate_name,
+                "rate" => $tax->tax_rate,
+                "class" => $tax->tax_rate_class
+            );
+
+            // Add tax rate info to the array
+            $all_tax_rate_info[] = $tax_rate_info;
         }
     }
-        return json_encode($all_tax_rate_ids);
-    
+
+    // Return the tax rate info as a JSON encoded string
+    return json_encode($all_tax_rate_info);
 }
-    
 
+// Example usage: Echo the result of get_txs function
 
-
-            // echo "<h2>Tax Class: $class</h2>";
-            // echo json_encode($taxes);
-            // echo "<br>";
-
-//             if (!empty($taxes)) {
-//                 foreach ($taxes as $tax) {
-//                     $all_tax_rate_ids['foreign_id']=$tax->tax_rate_id,
-//                     $all_tax_rate_ids['rate']=$tax->tax_rate,
-//                     $all_tax_rate_ids['name']=$tax->tax_rate_name,
-//                     $all_tax_rate_ids['class']=$tax->tax_rate_class,
-//                     // Extract tax rate ID, country, and postcodes
-//                     // $postcodes = is_array($tax->postcode) ? $tax->postcode : array($tax->postcode);
-//                     // $postcode_array = array();
-//                     // foreach ($postcodes as $postcode) {
-//                     //     $postcode_array[] = $postcode;
-//                     // }
-//                     // $cities = is_array($tax->city) ? $tax->city : array($tax->city);
-//                     // $city_array = array();
-//                     // foreach ($cities as $city) {
-//                     //     $city_array[] = $city;
-//                     // }
-
-//                     $tax_rate_info[] = array(
-//                         "id" => $tax->tax_rate_id,
-//                         "country" => $tax->tax_rate_country,
-//                         "state" => $tax->tax_rate_state,
-//                         "postcodes" => $postcode_array,
-//                         "cities" => $city_array,
-//                         "rate" => $tax->tax_rate,
-//                         "name" => $tax->tax_rate_name,
-//                         "priority" => $tax->tax_rate_priority,
-//                         "compound" => $tax->tax_rate_compound,
-//                         "shipping" => $tax->tax_rate_shipping,
-//                         "order" => $tax->tax_rate_order,
-//                         "class" => $tax->tax_rate_class
-//                     );
-//                 }
-//             } else {
-//                 // If no tax rates found for class, add custom attribute
-//                 $tax_rate_info[] = array(
-//                     "name" => $class,
-//                 );
-//             }
-
-//             // Add tax rate info for the current tax class to the $all_tax_rate_ids array
-//             $all_tax_rate_ids[] = $tax_rate_info;
-//         }
-//     } 
-//     return json_encode($all_tax_rate_ids);
-
-//     // Output all tax rate IDs with country in the desired format
-//     // echo "<h2>All Tax Rate IDs with Country</h2>";
-//     // echo json_encode($all_tax_rate_ids);
-//     // echo "<br>";
-// }
 
 ?>
