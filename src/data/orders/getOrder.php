0e3d95527d679807f686c3051078d61dd25d77cc
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-05-24 11:00:09 +0100

    get orders

--- src/data/orders/getOrder.php
+++ src/data/orders/getOrder.php
@@ -1,6 +1,5 @@
 <?php
 
-
 function get_odr() {
     global $wpdb;
     $args = array(
@@ -17,8 +16,7 @@ function get_odr() {
         // Retrieve order properties with null coalescing operator
         $order_data['foreign_id'] = $order->get_id() ?? null;
         $order_data['reference'] = $order->get_id() ?? null;
-    
-       
+
         $order_data['woo_status'] = method_exists($order, 'get_status') ? $order->get_status() : null;
         $order_data['woo_currency'] = method_exists($order, 'get_currency') ? $order->get_currency() : null;
         $order_data['invoice_date'] = method_exists($order, 'get_date_created') ? ($order->get_date_created() ? $order->get_date_created()->format('Y-m-d H:i:s.u') : null) : null;
@@ -29,12 +27,7 @@ function get_odr() {
         $order_data['customer_id'] = method_exists($order, 'get_customer_id') ? $order->get_customer_id() : null;
         $order_data['payment'] = method_exists($order, 'get_payment_method_title') ? $order->get_payment_method_title() : null;
         $order_data['note'] = method_exists($order, 'get_customer_note') ? $order->get_customer_note() : null;
-       
         $order_data['delivery_date'] = method_exists($order, 'get_date_completed') ? ($order->get_date_completed() ? $order->get_date_completed()->format('Y-m-d H:i:s.u') : null) : null;
-
-
-    
-        $product_items = [];
 
         // Check if the order object has a method 'get_items' and retrieve the items
         if (method_exists($order, 'get_items')) {
@@ -45,35 +38,10 @@ function get_odr() {
         
         // Count the number of items
         $order_data['total_products'] = count($items);
-        $order_data['total_products_wt'] = count($items);
-        
-        
-       
-
-
-
-
-
-  
-
-
-
-
-
-
-
 
         $orders_data[] = $order_data;
-
-
     }
-
-
-
 
     return json_encode($orders_data);
 }
-
-
-
 ?>
