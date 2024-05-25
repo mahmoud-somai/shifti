ca9148fa37cc7693750a47c6d98e68f58f6e7442
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-05-25 15:18:09 +0100

    get orders details

--- src/data/orders/getOrderDetails.php
+++ src/data/orders/getOrderDetails.php
@@ -8,6 +8,12 @@ function get_ord_det() {
     $orders = $orders_query->get_orders();
     $line_items_data = [];
     foreach ($orders as $order) {
+        // Check if customer_id is not 0 or null
+        $customer_id = method_exists($order, 'get_customer_id') ? $order->get_customer_id() : null;
+        if ($customer_id === 0 || $customer_id === null) {
+            continue;
+        }
+        
         $order_id = $order->get_id();
         $items = $order->get_items();
         foreach ($items as $item) {
