0c48a97c77d0eaed95b98605aaf109ce82e605c9
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-05-27 11:56:13 +0100

    export orders details

--- shifti_import.php
+++ shifti_import.php
@@ -92,6 +92,13 @@ function download_orders_details_json() {
     exit();
 }
 
+add_action('wp_ajax_get_orders_details_data', 'get_orders_details_data');
+function get_orders_details_data() {
+    $json_data = get_ord_det();  
+
+    wp_send_json_success($json_data);
+}
+
 add_action('wp_ajax_download_orders_fees_json', 'download_orders_fees_json');
 function download_orders_fees_json() {
 
