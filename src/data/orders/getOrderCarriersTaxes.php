77782888f06e74fc45ac7866d3e311ba04faf377
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-06-04 22:44:13 +0100

    new version

--- src/data/orders/getOrderCarriersTaxes.php
+++ src/data/orders/getOrderCarriersTaxes.php
@@ -17,14 +17,23 @@ function get_ord_car_tx() {
 
         $order_id = $order->get_id();
         $shipping_items = $order->get_items('shipping');
+        $tax_items = $order->get_items('tax');
+
+        
+            // foreach ($tax_items as $item_id => $item) {  
+            //     $tax_item_rate_id = $item->get_rate_id();
+            // }
+
         foreach ($shipping_items as $item_id => $item) {
             $id = method_exists($item, 'get_id') ? $item->get_id() : null;
 
             $total   = method_exists($item,'get_total_tax') ? $item->get_total_tax():nul;
+            $tax_item_rate_id = $item->get_rate_id();
             
             $line_item = array(
                 'foreign_id' => $id,
                 'total' =>floatval($total),
+                'tax_id' => $tax_item_rate_id,s
                 
             );
             $line_items_data[] = $line_item;
