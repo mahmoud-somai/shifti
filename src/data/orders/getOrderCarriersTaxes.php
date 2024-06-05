5dd8e294611a6257507dfef1e68276890f2e16bb
MahmoudSomai <somai.mahmoud096@gmail.com>
2024-06-05 23:57:42 +0100

    new version

--- src/data/orders/getOrderCarriersTaxes.php
+++ src/data/orders/getOrderCarriersTaxes.php
@@ -41,7 +41,7 @@ function get_ord_car_tx() {
             $line_items_data[] = $line_item;
         }
     }
-    // echo json_encode($line_items_data);
+    echo json_encode($line_items_data);
     return json_encode($line_items_data);
 }
 ?>
